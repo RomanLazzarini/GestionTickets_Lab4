6f6aa5b0e377dc86a685707fae0fd2248522bc78
Roman Lazzarini <lazzariniroman@gmail.com>
2026-02-15 22:43:26 -0300

    Estilos completos de Auth (Login/Register/Recovery) y corrección de namespaces.

---
+++ GestionTickets_Lab4/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestionTickets_Lab4.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Método vacío, solo carga la vista
+        }
+    }
+}
